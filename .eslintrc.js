6f2781b999f3aefd0e4475f9a2bcf37d0286f67d
ousf <ousf@mail.com>
2022-12-04 16:56:57 +0100

    init linting

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:import/recommended",
+    "prettier",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["import", "prettier"],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "import/no-unresolved": [2, { commonjs: true, amd: true }],
+    "import/named": 2,
+    "import/namespace": 2,
+    "import/default": 2,
+    "import/export": 2,
+
+    quotes: ["warn", "double"],
+    "no-console": "warn",
+    semi: ["error", "always"],
+    "comma-spacing": ["warn", { before: false, after: true }],
+    "semi-spacing": ["error", { before: false, after: true }],
+    "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+    "switch-colon-spacing": ["error", { after: true, before: false }],
+    "object-curly-spacing": ["error", "always", { objectsInObjects: true }],
+    "multiline-ternary": ["error", "always-multiline"],
+    "arrow-body-style": ["error", "as-needed"],
+    "no-empty": "error",
+    "no-unused-vars": [
+      "error",
+      { vars: "all", args: "after-used", ignoreRestSiblings: false },
+    ],
+    "no-undef": "error",
+    curly: ["error", "multi-or-nest", "consistent"],
+  },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  printWidth: 150,
+  trailingComma: "es5",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: false,
+  arrowParens: "avoid",
+};
